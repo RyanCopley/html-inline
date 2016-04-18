bbf556fcd7a6c3b82f51c1000c8b8e8ff0636924
Ryan Copley <github@ryancopley.com>
2016-04-18 19:16:49 -0400

    3rd commit, i like numbering my commits

--- inliner.js
+++ inliner.js
@@ -3,10 +3,19 @@
 var request = require("request").defaults({ encoding: null });
 var cheerio = require('cheerio');
 var url = require("url");
+var path = require("path");
 var _ = require("lodash");
 var async = require("async");
-var uglify = require("uglify-js");
-var base64 = require("base-64");
+var mime = require('mime-types')
+var minify = require('html-minifier').minify;
+var minifyOpts = {
+	removeComments : true,
+	collapseWhitespace : true,
+	minifyCSS : true,
+	minifyJS : true,
+	processScripts : [],
+	removeOptionalTags : true
+};
 
 function inline(startAddress, cb) {
 	var urlComponents = url.parse(startAddress);
@@ -15,16 +24,18 @@ function inline(startAddress, cb) {
 
 	download(startAddress, function (err, body){
 		var $ = cheerio.load(body);
-		removeIncompatibles($, context, function (err){
-			stripLinks($, context, function (err){
-				scanScripts($, context, function (err){
-					scanStyles($, context, function (err){
-						scanImages($, context, function (err){
-							cb(null, $.html());
-						});
-					});
-				});
-			});
+		var runlist = [
+			removeIncompatibles,
+			stripLinks,
+			scanScripts,
+			scanStyles,
+			scanImages
+		];
+
+		async.each(runlist, function (fn, done){
+			fn($, context, done);
+		}, function (err){
+			cb(null, minify($.html(), minifyOpts));
 		});
 	});
 }
@@ -37,9 +48,10 @@ function removeIncompatibles($, context, cb){
 function stripLinks($, context, cb){
 	async.each($("a"), function (tag, done){
 		var obj = cheerio(tag);
+		var sourceLink = obj.attr("href");
 		var sourceString = obj.text();
 
-		obj.replaceWith($("<span>").text(sourceString));
+		obj.replaceWith($("<span>").text(sourceString).attr("originalUrl", sourceLink).addClass("inline-hidden-link"));
 		return done();
 	}, cb);
 }
@@ -47,16 +59,9 @@ function stripLinks($, context, cb){
 function scanScripts($, context, cb){
 	async.each($("script"), function (tag, done){
 		var obj = cheerio(tag);
-		var sourceString = obj.attr("src");
+		var sourceString = fullyQualifyUrl(obj.attr("src"), context);
 		if (!sourceString){
 			return done();
-		}
-
-		var scriptSourceUrl = url.parse(sourceString);
-		
-		// Fully qualified URLs
-		if (!scriptSourceUrl.host){
-			sourceString = context.url.protocol + "//" + context.url.host + sourceString;
 		}
 		
 		download(sourceString, function (err, body){
@@ -69,21 +74,16 @@ function scanScripts($, context, cb){
 function scanStyles($, context, cb){
 	async.each($("link"), function (tag, done){
 		var obj = cheerio(tag);
-		var sourceString = obj.attr("href");
+		var sourceString = fullyQualifyUrl(obj.attr("href"), context);
 		if (!sourceString){
 			return done();
 		}
-
-		var scriptSourceUrl = url.parse(sourceString);
-		
-		// Fully qualified URLs
-		if (!scriptSourceUrl.host){
-			sourceString = context.url.protocol + "//" + context.url.host + sourceString;
-		}
 		
 		download(sourceString, function (err, body){
-			obj.replaceWith($("<style>").html(body));
-			return done();
+			inlineCssRefs(body, sourceString, function (err, inlinedCss){
+				obj.replaceWith($("<style>").html(inlinedCss));
+				return done();
+			});
 		});
 	}, cb);
 }
@@ -91,26 +91,58 @@ function scanStyles($, context, cb){
 function scanImages($, context, cb){
 	async.each($("img"), function (tag, done){
 		var obj = cheerio(tag);
-		var sourceString = obj.attr("src");
-		console.log(sourceString);
+		var sourceString = fullyQualifyUrl(obj.attr("src"), context);
 		if (!sourceString){
 			return done();
-		}
-
-		var scriptSourceUrl = url.parse(sourceString);
-		
-		// Fully qualified URLs
-		if (!scriptSourceUrl.host){
-			sourceString = context.url.protocol + "//" + context.url.host + sourceString;
 		}
 
 		download(sourceString, function (err, body){
 			var imageData = new Buffer(body, "binary");
 			var base64 = imageData.toString("base64");
-			obj.attr("src", "data:image/jpeg;charset=utf-8;base64," + base64);
+			obj.attr("src", "data:" + mime.lookup(sourceString) + ";charset=utf-8;base64," + base64);
 			return done();
 		});
 	}, cb);
+}
+
+function fullyQualifyUrl(urlString, context){
+	if (!urlString){
+		return "";
+	}
+
+	var scriptSourceUrl = url.parse(urlString);
+	
+	// Fully qualified URLs
+	if (!scriptSourceUrl.host){
+		urlString = context.url.resolve(urlString);
+	}
+	return urlString;
+}
+
+function inlineCssRefs(css, originatorUrl, done){
+	css = css.toString();
+	var urlRegex = /url\s?\([\S^\)]+\)/g
+	var matches = css.match(urlRegex);
+
+	async.each(matches, function (original, done){
+		var urlPath = original.replace("url", "").replace("(","").replace("'", "").replace("'", "").replace(")","");
+
+		var urlComponents = url.parse(originatorUrl);
+		var context = {};
+		context.url = urlComponents;
+		var resourceString = fullyQualifyUrl(urlPath, context);
+
+		download(resourceString, function (err, body){
+			var imageData = new Buffer(body, "binary");
+			var base64 = imageData.toString("base64");
+			var inlined = "url('data:" + mime.lookup(urlPath) + ";charset=utf-8;base64,"+base64+"')";
+			css = css.replace(original, inlined);
+			done();
+		});
+	}, function (err){
+		done(null, css);
+
+	});
 }
 
 function download(url, cb){	
