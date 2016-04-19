f9b1f7a982b98a5b56a72d01bc737de987355af5
Ryan Copley <github@ryancopley.com>
2016-04-18 22:36:31 -0400

    Cleaned up

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ var fs = require("fs");
 var path = require("path");
 var inliner = require("./inliner.js");
 var open = require("open");
-
 inliner.inlineByUrl("http://wlna-webservice.gannettdigital.com/articleservice/azcentral-sports/iphone/view/83160700/", function (err, doc){
-	var localStorage = path.join(__dirname, "output", Math.random()+".html");
-	fs.writeFile(localStorage, doc, function (err){
-		open(localStorage);
+	var path = path.join(__dirname, "output", Math.random()+".html");
+	fs.writeFile(path, doc, function (err){
+		open(path);
 		setTimeout(function(){
-			fs.unlink(localStorage);
-		}, 1000);
+			fs.unlink(path);
+		}, 100);
 	});
 });
